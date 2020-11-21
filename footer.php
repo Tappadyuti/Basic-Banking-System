c71d418ce83b25da2e85b095f69c726d4dad590a
Tappadyuti <50856707+Tappadyuti@users.noreply.github.com>
2020-11-21 22:34:57 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<title>Untitle document</title>
+</head>
+<body>
+<table width ="1316" bgcolor="purple" style=" color:white;bottom:0px;font-size:20px;">
+<tr>
+<td class="f" align="center" >Basic Banking System </td>
+</tr>
+</table>
+</html>
